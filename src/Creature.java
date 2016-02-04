a6fa8185324ef783e46d25a1c7cb508683cb9901
atillaahun <szikora.atilla@gmail.com>
2016-02-04 09:50:05 +0100

    finished

--- src/Creature.java
+++ src/Creature.java
@@ -1,4 +1,5 @@
 
 public interface Creature {
-    public void eat(String);
+    public void eat(String food, int quantity);
+    public void drink(String what);
 }
--- src/Human.java
+++ src/Human.java
@@ -1,5 +1,14 @@
 
-public class Human {
+public class Human implements Creature{
+
+    @Override
+    public void eat(String food, int quantity) {
+        weight += quantity;
+    }
+
+    @Override
+    public void drink(String what) {}
+
     enum Gender {MALE, FEMALE}
     Gender gender;
     String name;
@@ -16,7 +25,7 @@ public class Human {
         this.weight = weight;
         this.hasPrius = hasPrius;
     }
-
+    /*
     public Gender getGender() {return gender;}
     public void setGender(Gender gender) {this.gender = gender;}
     public String getName() {return name;}
@@ -29,14 +38,19 @@ public class Human {
     public void setWeight(float weight) {this.weight = weight;}
     public boolean isHasPrius() {return hasPrius;}
     public void setHasPrius(boolean hasPrius) {this.hasPrius = hasPrius;}
+    */
 
     @Override
     public String toString() {
-        return "Gender:" + gender.toString() + " Name: " + name + " Age: " + age;
+        return "Gender:" + gender.toString() + " Name: " + name + " Age: " + age + " Weight: " + weight;
     }
 
     public static void main(String[] args) {
         Human vakOnd = new Human(Gender.MALE, "Vak Ond", 11, 170, 60, false);
         System.out.println(vakOnd.toString());
+        Creature jooTas = new Human(Gender.MALE, "Joo Tas", 12, 180, 70, true);
+        System.out.println(jooTas);
+        jooTas.eat("Crap", 10);
+        System.out.println(jooTas);
     }
 }
